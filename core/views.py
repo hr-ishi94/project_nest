5de7d51fea0d03c35039b6dd880072c4e822d036
Hrishi <hrishikeshprasadc@gmail.com>
2023-10-30 22:32:26 +0530

    filter updation 1

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse
 from variant.models import VariantImage,Variant
 from products.models import Size,ProductReview,Product
 from django.db.models import Avg,Min,Max
@@ -7,41 +8,54 @@ from cart.models import *
 from wishlist.models import *
 
 from django.core.paginator import Paginator
-from django.http import JsonResponse
+from django.http import HttpResponseRedirect    
 from django.template.loader import render_to_string
 from .forms import PriceRangeForm
-
+from django.core import serializers
+import json
+from django.db.models import Q,F, Min,Max       
 
 
 def index(request):
+    categories = category.objects.all()
+    products = Product.objects.all()
     
-    categories=category.objects.all()
-    products=Product.objects.all()
-    min_price=Product.objects.aggregate(Min('product_price'))
-    max_price=Product.objects.aggregate(Max('product_price'))
+    # Retrieve the minimum and maximum prices
+    min_price = Product.objects.aggregate(Min('product_price'))
+    max_price = Product.objects.aggregate(Max('product_price'))
     
+    variant_images = VariantImage.objects.filter(variant__product__is_available=True).order_by('variant__product').distinct('variant__product')
+    
+    p = Paginator(VariantImage.objects.filter(variant__product__is_available=True).order_by('variant__product').distinct('variant__product'), 5)
+    page = request.GET.get('page')
+    product_page = p.get_page(page)
+    nums = "a" * product_page.paginator.num_pages
+    
+    filtered_products = request.session.get('filtered_products', [])
 
-
-    variant_images=VariantImage.objects.filter(variant__product__is_available=True).order_by('variant__product').distinct('variant__product')
-    p=Paginator(VariantImage.objects.filter(variant__product__is_available=True).order_by('variant__product').distinct('variant__product'), 5)
-    page=request.GET.get('page')
-    product_page=p.get_page(page)
-    nums="a"*product_page.paginator.num_pages
+    # Check if filtered_products is not an empty list
+    if filtered_products:
+        # Extract product IDs from the list of dictionaries
+        filtered_product_ids = [item.get('id') for item in filtered_products if 'id' in item]
+        
+        # Use the extracted product IDs to filter products
+        filtered_products = Product.objects.filter(id__in=filtered_product_ids)
+    else:
+        # If there are no filtered products, display all products
+        filtered_products = Product.objects.all()
     
-    context={
-        'categories':categories,
-        'products':products,
-        'variant_images':variant_images,
-        'product_page':product_page, 
-        'nums':nums,
-        'min_price':min_price,
-        'max_price':max_price,
+    context = {
+        'categories': categories,
+        'products': products,
+        'variant_images': variant_images,
+        'product_page': product_page,
+        'nums': nums,
+        'min_price': min_price,
+        'max_price': max_price,
+        'filtered_products': filtered_products,  # Send filtered products to the template
     }
-
-   
-
     
-    return render(request,'core/index.html',context)
+    return render(request, 'core/index.html', context)
 
 def product_show(request,prod_id,img_id):
   
@@ -103,31 +117,28 @@ def search_view(request):
     return render(request,'core/search.html',context)  
 
 
-# def filter_data(request):
-#     min=request.GET['minPrize']
-#     max=request.GET['maxPrize']
-#     allProducts= Product.objects.all().order_by('-id').distinct()
-#     allProducts=allProducts.filter(product_price__gte=min)
-#     allProducts=allProducts.filter(product_price__lte=max)
-#     t=render_to_string('ajax/product-list.html',{'data':allProducts})
-#     return JsonResponse({'data':t})
-
-
-from django.http import HttpResponseRedirect
-from django.urls import reverse
-
 def filter_data(request):
     if request.method == 'POST':
         form = PriceRangeForm(request.POST)
         if form.is_valid():
             min_price = form.cleaned_data['min_price']
             max_price = form.cleaned_data['max_price']
-            
-            # Store the filter criteria in the session
-            request.session['filter_min_price'] = min_price
-            request.session['filter_max_price'] = max_price
 
-    # Redirect back to the index view
+            print("Min Price:", min_price)
+            print("Max Price:", max_price)
+
+            # Filter products based on the price range
+            filtered_products = Product.objects.filter(
+                Q(product_price__gte=min_price) & Q(product_price__lte=max_price)
+            )
+            print("Filtered products:", filtered_products)
+
+            # Convert the QuerySet to a list of dictionaries
+            filtered_products_data = json.loads(serializers.serialize('json', filtered_products))
+
+            # Store the filtered products in the session
+            request.session['filtered_products'] = filtered_products_data
+
     return HttpResponseRedirect(reverse('index'))
 
 
